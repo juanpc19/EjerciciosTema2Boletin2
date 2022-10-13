76582c6f1b8af879bd4140b8b14cf2b802f97449
jperez <jperez@106-20.info2>
2022-10-13 14:31:15 +0200

    ej 4 y 5

---
+++ EjerciciosTema2Boletin2/src/ejerciciostema2boletin2/Ejercicio5.java
@@ -0,0 +1,43 @@
+package ejerciciostema2boletin2;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		
+		String tipoCarnet;
+		
+		Scanner dogma = new Scanner(System.in);
+		
+		System.out.print("Introduzca su tipo de carnet de conducir, use las opciones "
+				+ "indicadas a continuacion, E: remolques. D: autobuses.  \n"
+				+ "C1-C5: camiones. A: motocicletas. B1-B2: automóviles.");
+		
+		tipoCarnet = dogma.next();
+		
+		switch (tipoCarnet) {
+			case "E": 
+			System.out.print("Ese tipo de carnet pertenece a remolques");
+			break;
+			case "D": 
+			System.out.print("Ese tipo de carnet pertenece a autobuses");
+			break;
+			case "C1", "C2", "C3", "C4", "C5": 
+			System.out.print("Ese tipo de carnet pertenece a camiones");	
+			break;
+			case "A": 
+			System.out.print("Ese tipo de carnet pertenece a motocicletas");
+			break;
+			case "B1", "B2": 
+			System.out.print("Ese tipo de carnet pertenece a automoviles");
+			break;
+			default:
+			System.out.print("Error de seleccion, introduzca: E, D, C1, C2, C3, C4, C5, A, B1, B2");
+		}
+		
+		dogma.close();
+
+	}
+
+}
